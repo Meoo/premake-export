9895b905db4ab4649841d1b698647590b9120510
Bastien Brunnenstein <Meoo@users.noreply.github.com>
2015-08-13 18:23:44 +0200

    Replace tabs with 2 spaces

--- virtualproject.lua
+++ virtualproject.lua
@@ -7,35 +7,35 @@ local p = premake
 
 
 function virtualproject(name)
-	project(name)
-	p.api.scope.project.virtual = true
+  project(name)
+  p.api.scope.project.virtual = true
 end
 
 premake.override(p.main, "postBake", function(base)
-	print "Removing virtual projects..."
+  print "Removing virtual projects..."
 
-	for sln in p.global.eachSolution() do
-		local virtualProjects = {}
-		local toRemove = {}
+  for sln in p.global.eachSolution() do
+    local virtualProjects = {}
+    local toRemove = {}
 
-		for k, v in ipairs(sln.projects) do
-			if v.virtual then
-				table.insert(toRemove, k)
-				virtualProjects[v.name] = v
-			end
-		end
+    for k, v in ipairs(sln.projects) do
+      if v.virtual then
+        table.insert(toRemove, k)
+        virtualProjects[v.name] = v
+      end
+    end
 
-		for k, v in ipairs(toRemove) do
-			table.remove(sln.projects, v - (k - 1))
-		end
+    for k, v in ipairs(toRemove) do
+      table.remove(sln.projects, v - (k - 1))
+    end
 
-		sln.virtualProjects = virtualProjects
-	end
+    sln.virtualProjects = virtualProjects
+  end
 
-	return base()
+  return base()
 
 end)
 
-premake.override(p.solution, "findproject", function(base, self, name)
-	return base(self, name) or self.virtualProjects[name]
-end)
+-- premake.override(p.solution, "findproject", function(base, self, name)
+--   return base(self, name) or (self.virtualProjects or {})[name]
+-- end)
